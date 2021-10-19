31f56ff16e4878fd2ec98af4b48d206ca7e0dd0b
Hugo <jhugodc@gmail.com>
2021-10-19 08:30:59 -0600

    changes

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 if [ -f 'data/invalid.csv' ]; then
   timestamp=$(date +'%Y%m%d%H%M%s')
   mv ./data/invalid.csv ./data/invalid.$timestamp
+  mv ./data/invalid_txs.csv ./data/invalid_txs.$timestamp
 fi
 
 node main.js
