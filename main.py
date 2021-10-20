2ac1c4d0c86bda928d5c15442a9c81eca7ff1abb
Hugo <jhugodc@gmail.com>
2021-10-20 13:01:59 -0600

    Try to trace entire block

--- main.py
+++ main.py
@@ -25,40 +25,46 @@ if True:
     if current_block > last_block:
         time.sleep(1)
 
-    block = rpc.get_block_by_number(current_block)
-    total_txs = len(block['result']['transactions'])
-    print(current_block, '/', last_block, '-', total_txs, 'txs')
+    current_block_hex = hex(current_block)
+    print(current_block_hex)
+    result = rpc.debug_traceBlockByNumber(current_block_hex, tracer_script)
 
-    error_found = False
-    ix = 0
-    for tx in block['result']['transactions']:
-        ix += 1
-        print('|_', current_block, ':', ix, '/', total_txs)
-        trace = None
-        try:
-        #if True:
-            trace = rpc.debug_traceTransaction(tx['hash'], tracer_script)
-        #else:
-        except:
-            print('tr:', trace)
-            error_found = True
-            break
+    print(result)
 
-        if 'result' in trace.keys():
-            rs = trace['result']
-            block = rs['block']
-            invalid = rs['invalid']
-            if invalid:
-                #ops = [str((str(e[0]), e[1], str(e[2]))) for e in rs['ops']]
-                ops = [str(e) for e in rs['ops']]
-                line = [tx['blockNumber'], tx['hash'], tx['from'], tx['to'], tx['gas'], tx['gasPrice'], tx['transactionIndex'], str(rs['start_gas']), str(rs['end_gas']), str(rs['gas_used']), str(rs['intrinsic_gas']), str(rs['input_data_cost']), ','.join(ops) ]
-                print(line)
-                invalid_csv_file.write(','.join(line) + '\n')
-        else:
-            print('Err:', trace)
-            error_found = True
-    #if error_found:
-    #    print('error found!')
-    #    continue
+    #block = rpc.get_block_by_number(current_block)
+    #total_txs = len(block['result']['transactions'])
+    #print(current_block, '/', last_block, '-', total_txs, 'txs')
 
-    #current_block += 1
+    #error_found = False
+    #ix = 0
+    #for tx in block['result']['transactions']:
+    #    ix += 1
+    #    print('|_', current_block, ':', ix, '/', total_txs)
+    #    trace = None
+    #    try:
+    #    #if True:
+    #        trace = rpc.debug_traceTransaction(tx['hash'], tracer_script)
+    #    #else:
+    #    except:
+    #        print('tr:', trace)
+    #        error_found = True
+    #        break
+
+    #    if 'result' in trace.keys():
+    #        rs = trace['result']
+    #        block = rs['block']
+    #        invalid = rs['invalid']
+    #        if invalid:
+    #            #ops = [str((str(e[0]), e[1], str(e[2]))) for e in rs['ops']]
+    #            ops = [str(e) for e in rs['ops']]
+    #            line = [tx['blockNumber'], tx['hash'], tx['from'], tx['to'], tx['gas'], tx['gasPrice'], tx['transactionIndex'], str(rs['start_gas']), str(rs['end_gas']), str(rs['gas_used']), str(rs['intrinsic_gas']), str(rs['input_data_cost']), ','.join(ops) ]
+    #            print(line)
+    #            invalid_csv_file.write(','.join(line) + '\n')
+    #    else:
+    #        print('Err:', trace)
+    #        error_found = True
+    ##if error_found:
+    ##    print('error found!')
+    ##    continue
+
+    ##current_block += 1
